10d318a515a2781541b14f5f2c804c9e0f4e6011
Specii <toatikan@gmail.com>
2026-02-08 18:47:46 +0000

    More Main Menu PEAK changes

--- 7-minutes-before-death/Scripts/AnimationScripts/corridor.gd
+++ 7-minutes-before-death/Scripts/AnimationScripts/corridor.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 var blocks = 0
+var voidblocks = 0
 var corridor_block = preload("res://Scenes/corridor_block.tscn")
+var void_blocks = preload("res://Scenes/void.tscn")
 var start_block
+var void_block_start 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +14,11 @@ func _ready() -> void:
 		start_block.name = "Corridor" + str(blocks)
 		self.add_child(start_block)
 		blocks+=1
-	
+	if !$void:
+		void_block_start = void_blocks.instantiate()
+		void_block_start.name = "void" + str(blocks)
+		self.add_child(void_block_start)
+		voidblocks+=1
 	pass # Replace with function body.
 
 
@@ -23,4 +30,10 @@ func _process(delta: float) -> void:
 		start_block.global_position = Vector2(64,-32)*20
 		self.add_child(start_block)
 		blocks+=1
+	if voidblocks < 2:
+		void_block_start = void_blocks.instantiate()
+		void_block_start.name = "Corridor" + str(voidblocks)
+		void_block_start.global_position = Vector2(64,-32)*20
+		self.add_child(void_block_start)
+		voidblocks+=1
 	pass
--- 7-minutes-before-death/Scripts/Multiplayer/multiplayer_manager.gd
+++ 7-minutes-before-death/Scripts/Multiplayer/multiplayer_manager.gd
@@ -36,9 +36,9 @@ func joinHostGame():
 func _add_player_to_game(id: int):
 	print("Player %s joined the game!" % id)
 	if id == 1:
-		get_tree().get_current_scene().get_node("MainMenuCamera/Gurney/GurneyP1").frame = 1
+		get_tree().get_current_scene().get_node("MainMenuCamera/Sprites/Gurney/GurneyP1").frame = 1
 	else:
-		get_tree().get_current_scene().get_node("MainMenuCamera/GuardianP2/guaP2").show()
+		get_tree().get_current_scene().get_node("MainMenuCamera/Sprites/GuardianP2/guaP2").show()
 	var targetPlayer = multiplayer_scene.instantiate()
 	targetPlayer.player_id = id
 	targetPlayer.name = str(id)

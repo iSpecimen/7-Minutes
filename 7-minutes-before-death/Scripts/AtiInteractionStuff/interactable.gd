73ddc4a74659f05a09403d2db24758abf5b31b98
iSpecimen <iSpecimenCreatorAcc@gmail.com>
2026-02-10 02:17:41 +0000

    ez pop up woah

--- 7-minutes-before-death/Scripts/AtiInteractionStuff/interactable.gd
+++ 7-minutes-before-death/Scripts/AtiInteractionStuff/interactable.gd
@@ -3,34 +3,39 @@ extends Node2D
 @export var ui_scene: PackedScene
 
 var player_in_range := false
-var ui_instance: Control
+var pop_up_UI: Control
 
 func _ready():
-	if ui_scene:
-		ui_instance = ui_scene.instantiate()
-		get_tree().root.add_child(ui_instance)
-		ui_instance.visible = false
+	pass
 
 func _input(event):
 	if player_in_range and event.is_action_pressed("interact"):
+		
 		toggle_ui()
 
 func toggle_ui():
-	if not ui_instance:
-		return
-
-	ui_instance.visible = !ui_instance.visible
-	get_tree().paused = ui_instance.visible
+	if $/root/Game.popUpOpen:
+		$/root/Game.popUpOpen = false
+		print("Toggle UI Off")
+		$/root/Game/MainMenuCamera/PopUpUI.hide()
+	else:
+		$/root/Game.popUpOpen = true
+		print("Toggle UI On")
+		$/root/Game/MainMenuCamera/PopUpUI.show()
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		print("Player Entered Interact Area")
 		player_in_range = true
-		$InteractUITest.visible = true
+		$InteractLabel.show()
 
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group("player"):
 		print("Player Left Interact Area")
 		player_in_range = false
-		$InteractUITest.visible = false
+		$InteractLabel.hide()
+		if $/root/Game.popUpOpen:
+			$/root/Game.popUpOpen = false
+			print("Left Popup Area")
+			$/root/Game/MainMenuCamera/PopUpUI.hide()
